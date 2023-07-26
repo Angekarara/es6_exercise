df608f7a475756f9552951ed8829e1aca15fea60
Angekarara <mbabange2020@gmail.com>
2023-07-26 11:48:57 +0200

    template literal

--- exercise1.js
+++ exercise1.js
@@ -6,7 +6,12 @@ const teacher = {
   name: 'muhizi',
   subject: 'Math',
 };
-let text =  `${student.name} your age is ${student.age} and your teacher is ${teacher.name} the subject is ${teacher.subject}`
+let text =  `${student.name},
+ your age is ${student.age} and,
+
+your teacher is ${teacher.name}.
+
+the subject is ${teacher.subject}`
 console.log(text);
  
 
