e6d70d62a8fae055b21cfe029907c12241007d9a
Angekarara <mbabange2020@gmail.com>
2023-07-26 11:01:08 +0200

    first commit

---
+++ exercise1.js
@@ -0,0 +1,20 @@
+const student = {
+  name: 'ange',
+  age: 12,
+};
+const teacher = {
+  name: 'muhizi',
+  subject: 'Math',
+};
+
+console.log(
+  "dear " +
+    student.name +
+    " age of " +
+    student.age +
+    " your teacher " +
+    teacher.name +
+    " subject of " +  
+    teacher.subject + 
+    " needs to see you "
+);
