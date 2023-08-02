86888dff0dbf19c3f663ae0de857688efb395045
Angekarara <mbabange2020@gmail.com>
2023-08-02 13:22:38 +0200

    looping through sets

--- set.js
+++ set.js
@@ -3,3 +3,14 @@ console.log(games.add(8))
 console.log(games.delete(3))
 console.log(games.size)
 console.log(games.values())
+//looping throug sets using for..of
+for(let game of games){
+console.log(game)
+}
+//looping through sets using the way of object iterator
+const valy = games.values();
+console.log(valy.next())
+console.log(valy.next())
+console.log(valy.next())
+console.log(valy.next())
+console.log(valy.next())
