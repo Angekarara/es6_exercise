aac93bd4273fd45c20289d6de2928f304d58fcc1
Angekarara <mbabange2020@gmail.com>
2023-08-02 18:00:00 +0200

    map

---
+++ maps.js
@@ -0,0 +1,16 @@
+const employees = new Map()
+employees.set('kevine' ,{
+    age:22,
+    sex: 'female'
+})
+
+employees.set('ange', {
+    age: 23,
+    sex:'male'
+})
+const add = employees.has('ange')
+console.log(employees)
+console.log(employees.get('kevine'))
+for (let employee of employees){
+    console.log(employee)
+}
